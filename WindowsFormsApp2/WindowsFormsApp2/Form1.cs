dd27aad994151711cc52513d697dcc449d3fb74b
kleberposligua <kleberposligua@hotmail.com>
2019-10-30 10:57:19 -0500

    segundo commit

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola2");
+        }
     }
 }
